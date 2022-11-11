1d18bb58a065bf17e1c0990add4486c9693a37f6
Dawn Arrington <dawn.arrington@gmail.com>
2022-11-10 17:35:57 -0800

    Deliverable for Project4

--- app.py
+++ app.py
@@ -23,6 +23,54 @@ def root():
 
     return render_template("index.j2")
 
+@app.route('/customers', methods=["POST", "GET", "DELETE"])
+def customers():
+    if request.method == "GET":
+        query = "SELECT * FROM Customers;"
+        cursor = db.execute_query(db_connection=db_connection, query=query)
+        results = cursor.fetchall()
+        return render_template("customers.j2", customers=results)
+
+    if request.method == "POST":
+        if request.form.get("addCustomer"):
+            firstName = request.form["firstName"]
+            lastName = request.form["lastName"]
+            phoneNumber = request.form["phoneNumber"]
+            email = request.form["email"]
+            if firstName != '' and lastName != '' and phoneNumber != '' and email != '':
+                query = "INSERT INTO Customers (firstName, lastName, phoneNumber, email) VALUES (%s, %s, %s, %s);"
+                cursor = db_connection.cursor()
+                cursor.execute(query, (firstName, lastName, phoneNumber, email,))
+                db_connection.commit()
+
+        return redirect("/customers")
+
+@app.route('/edit_customer/<int:customerID>', methods=["POST", "GET"])
+def edit_customer(customerID):
+
+    if request.method == "GET":
+        query = "SELECT * FROM Customers WHERE customerID = %s" % (customerID)
+        # cur = db_connection.cursor()
+        # cur.execute(query)
+        # data = cur.fetchall()
+        cursor = db.execute_query(db_connection=db_connection, query=query)
+        data = cursor.fetchall()
+        return render_template("edit_customer.j2", customers=data)
+
+    if request.method == "POST":
+        if request.form.get("editCustomer"):
+            firstName = request.form["firstName"]
+            lastName = request.form["lastName"]
+            phoneNumber = request.form["phoneNumber"]
+            email = request.form["email"]
+
+            query = "UPDATE Customers SET Customers.firstName = %s, Customers.lastName = %s, Customers.phoneNumber = %s, Customers.email = %s WHERE Customers.customerID = %s;"
+            print(query)
+            cur = db_connection.cursor()
+            cur.execute(query, (firstName, lastName, phoneNumber, email, customerID))
+            db_connection.commit()
+        return redirect("/customers")
+
 @app.route('/cars', methods=["POST", "GET", "DELETE"])
 def cars():
 
