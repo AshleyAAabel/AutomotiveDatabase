4bb8908f1f9e3aa3701f63ba93cb8bfd7343c877
Dawn Arrington <dawn.arrington@gmail.com>
2022-11-13 21:28:55 -0800

    functional Cars, Recalls, Customers and Customers Vehicles entities

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ mysql = MySQL(app)
 
 @app.route('/')
 def root():
-
     return render_template("index.j2")
 
 @app.route('/customers', methods=["POST", "GET", "DELETE"])
@@ -117,13 +116,109 @@ def delete_cars(carModelID):
 
     return redirect("/cars")
 
+@app.route('/recalls', methods=["POST", "GET"])
+def recalls():
+    if request.method == "GET":
+        query = "SELECT * FROM Recalls;"
+        # cursor = db.execute_query(db_connection=db_connection, query=query)
+        cursor = mysql.connection.cursor()
+        cursor.execute(query)
+        results = cursor.fetchall()
+        return render_template("recalls.j2", recalls=results)
+
+    if request.method == "POST":
+        if request.form.get("addRecalls"):
+            recallType = request.form["recallType"]
+            dateIssued = request.form["dateIssued"]
+            if recallType != '' and dateIssued != '':
+                query = "INSERT INTO Recalls (recallType, dateIssued) VALUES (%s, %s);"
+                # cursor = db_connection.cursor()
+                cursor = mysql.connection.cursor()
+                cursor.execute(query, (recallType, dateIssued,))
+                # db_connection.commit()
+                mysql.connection.commit()
+        return redirect("/recalls")
+
+@app.route('/customers_vehicles', methods=["POST", "GET", "DELETE"])
+def customers_vehicles():
+    if request.method == "GET":
+        
+        # cursor = db.execute_query(db_connection=db_connection, query=query)
+        customers_query = "SELECT * FROM Customers ORDER BY Customers.firstName;"
+        customers_cursor = mysql.connection.cursor()
+        customers_cursor.execute(customers_query)
+        customers_results = customers_cursor.fetchall()
+
+        cars_query = "SELECT * FROM Cars ORDER BY Cars.carMake;"
+        cars_cursor = mysql.connection.cursor()
+        cars_cursor.execute(cars_query)
+        cars_results = cars_cursor.fetchall()
+
+        query = "SELECT CustomersVehicles.customerVehicleID, CustomersVehicles.vinNumber, CustomersVehicles.saleDate, CustomersVehicles.lastServiceDate, Customers.firstName, Customers.lastName, Customers.phoneNumber, Cars.carMake, Cars.carModel, Cars.carYear FROM CustomersVehicles INNER JOIN Customers ON CustomersVehicles.customerID = Customers.customerID INNER JOIN Cars ON CustomersVehicles.carModelID = Cars.carModelID;"
+        cursor = mysql.connection.cursor()
+        cursor.execute(query)
+        results = cursor.fetchall()
+
+        return render_template("customers_vehicles.j2", customersVehicles=results, cars=cars_results, customers=customers_results)
+
+    if request.method == "POST":
+        if request.form.get("addCustomersVehicle"):
+            customerID = request.form["customerID"]
+            carModelID = request.form["carModelID"]
+            vinNumber = request.form["vinNumber"]
+            saleDate = request.form["saleDate"]
+            lastServiceDate = request.form["lastServiceDate"]
+            if customerID != '' and carModelID != '' and vinNumber != '' and saleDate != '' and lastServiceDate != '':
+                query = "INSERT INTO CustomersVehicles (customerID, carModelID, vinNumber, saleDate, lastServiceDate) VALUES (%s, %s, %s, %s, %s);"
+                print("DEBUGGG" + customerID + carModelID + vinNumber + saleDate + lastServiceDate)
+                # cursor = db_connection.cursor()
+                cursor = mysql.connection.cursor()
+                cursor.execute(query, (customerID, carModelID, vinNumber, saleDate, lastServiceDate,))
+                # db_connection.commit()
+                mysql.connection.commit()
+        return redirect("/customers_vehicles")
+
+@app.route('/edit_customers_vehicle/<int:customerVehicleID>', methods=["POST", "GET"])
+def edit_customers_vehicles(customerVehicleID):
+    if request.method == "GET":
+        query = "SELECT * FROM CustomersVehicles WHERE customerVehicleID = %s;" % (customerVehicleID)
+        # cursor = db.execute_query(db_connection=db_connection, query=query)
+        cursor = mysql.connection.cursor()
+        cursor.execute(query)
+        results = cursor.fetchall()
+        return render_template("edit_customers_vehicle.j2", customersVehicle=results)
+
+    if request.method == "POST":
+        if request.form.get("editCustomersVehicle"):
+            lastServiceDate = request.form["lastServiceDate"]
+            if lastServiceDate != '':
+                query = "UPDATE CustomersVehicles SET CustomersVehicles.lastServiceDate = %s WHERE CustomersVehicles.customerVehicleID = %s;"
+                # cursor = db_connection.cursor()
+                cursor = mysql.connection.cursor()
+                cursor.execute(query, (lastServiceDate, customerVehicleID,))
+                # db_connection.commit()
+                mysql.connection.commit()
+        return redirect("/customers_vehicles")
+
+@app.route('/delete_customers_vehicle/<int:customerVehicleID>')
+def delete_customers_vehicle(customerVehicleID):
+
+    query = "DELETE FROM CustomersVehicles WHERE customerVehicleID = '%s';"
+    # cursor = db_connection.cursor()
+    cursor = mysql.connection.cursor()
+    cursor.execute(query, (customerVehicleID,))
+    # db_connection.commit()
+    mysql.connection.commit()
+
+    return redirect("/customers_vehicles")
+
 # Listener
 
 if __name__ == "__main__":
 
 
 
-    port = int(os.environ.get('PORT', 6767)) 
+    port = int(os.environ.get('PORT', 6768)) 
     #                                 ^^^^
     #              You can replace this number with any valid port
     
