41f3903dbb9293fba04c5295ed7b10184135fc22
Dawn Arrington <dawn.arrington@gmail.com>
2022-11-17 16:17:27 -0800

    spacings between the links and the tables

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
 from flask import Flask, render_template, request, redirect, json, flash
-from waitress import serve
+# from waitress import serve
 import os
 import database.db_connector as db
 import mysql.connector
